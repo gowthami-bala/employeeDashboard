a0830674a5a01d63350b2c2cee401f06918cbafb
gowthami-bala <kvedantam@miraclesoft.com>
2023-09-18 22:37:51 +0530

    register and login template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,15 @@ import { MatSliderModule } from '@angular/material/slider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
-
+import {ButtonModule} from 'primeng/button';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {CardModule} from 'primeng/card';
+import {MatCardModule} from '@angular/material/card';
+import {InputTextModule} from 'primeng/inputtext';
+import {MatRadioModule} from '@angular/material/radio';
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,6 +23,11 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
     MatButtonModule,
     BrowserAnimationsModule,
     MatButtonToggleModule,
+    ButtonModule,
+    CardModule,
+    MatCardModule,
+    InputTextModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent],
